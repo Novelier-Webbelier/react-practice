418d36957324fd452f78d7f70b1bbc2f538b6b6c
Novelier-Webbelier <novelier.webbelier@gmail.com>
2022-06-02 22:10:52 +0900

    2.2 Our First React Element

--- sample/react/app.js
+++ sample/react/app.js
@@ -1,1 +1,14 @@
-console.log(React);
+const root = document.querySelector("#root");
+const span = React.createElement(
+  "span",
+  {
+    id: "sexy-span",
+    class: "sexy-span",
+    style: {
+      color: "red",
+    },
+  },
+  "Hello World! I am a span"
+);
+
+ReactDOM.render(span, root);
